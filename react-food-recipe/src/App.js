3c53c3be59dc342898e2d188fbb02f61ec900ab3
akash <akashdhiman716gmail.com>
2020-10-01 20:41:20 +0530

    new update project

---
+++ react-food-recipe/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Recipes from "./components/Recipes";
+import Axios from "axios";
+
+function App() {
+    const [search, setSearch] = useState();
+    const [recipes, setRecipes] = useState([]);
+
+    const App_ID = "f479120c";
+    const App_key = "caed7f8340f01ca9fd0896e9f5adfe21 ";
+
+    useEffect(() => {
+        getRecipes();
+    }, []);
+
+    
+    const getRecipes = async() => {
+        const res = await Axios.get(`https://api.edamam.com/search?q=${search}&app_id=${App_ID}&app_key=${App_key}`);
+         
+         setRecipes(res.data.hits);
+        
+        }
+    const onInputChange = e => {
+        setSearch(e.target.value);
+    }
+    const onSearchClick = () => {
+        getRecipes();
+    }
+    return ( <div className = "App" >
+
+        <Header search = { search }
+        onInputChange = { onInputChange }
+        onSearchClick = { onSearchClick }/>
+         <div className = "container" > < Recipes recipes = { recipes }/> </div > </div >
+    );
+
+}
+export default App;
---
+++ react-food-recipe/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Header = (props) =>{
+  const{search, onInputChange, onSearchClick}=props;
+    return (<div className="jumbotron">
+        <h1 className="display-1">
+            <span className = "material-icons brand-icon"> fastfood</span> AKS Food Recipie</h1>
+        <div className="input-group w-50 mx-auto">
+  <input type="text" className="form-control" placeholder="Search Your Recipe..." value={search} onChange={onInputChange}/>
+  <div className="input-group-append"/>
+    <button className="btn btn-dark" onClick = {onSearchClick}>Search Recipes</button>
+  </div>
+</div>
+    );
+};
+
+export default Header ;
