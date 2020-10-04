6dad75122e16637e103e63abb109a889dd0945de
akash <akashdhiman716gmail.com>
2020-10-04 18:59:08 +0530

    Updates

---
+++ precache-manifest.e631e9d7acb782d1050f2bbdea0ce4ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbf33bef76d89128848b499e1f2bc48",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "18928790fbee0baff594",
+    "url": "/food-recipe/static/css/main.3a4f8fbe.chunk.css"
+  },
+  {
+    "revision": "9797a24ce4c45584ca93",
+    "url": "/food-recipe/static/js/2.5c09c3c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-recipe/static/js/2.5c09c3c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18928790fbee0baff594",
+    "url": "/food-recipe/static/js/main.de06c4d8.chunk.js"
+  },
+  {
+    "revision": "0e60c34972797960b0d5",
+    "url": "/food-recipe/static/js/runtime-main.d2942620.js"
+  }
+]);
